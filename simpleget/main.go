7df7f3ef81bb963da79ed0179b0840d1b1826a4c
su-kun1899 <higedrum.coz@gmail.com>
2017-12-15 13:07:05 +0900

    3.5 GETメソッド＋クエリーの送信

--- simpleget/main.go
+++ simpleget/main.go
@@ -3,9 +3,13 @@ package main
 import "net/http"
 import "io/ioutil"
 import "log"
+import "net/url"
 
 func main() {
-	resp, err := http.Get("http://localhost:18888")
+	values := url.Values{
+		"query": {"hello world"},
+	}
+	resp, err := http.Get("http://localhost:18888" + "?" + values.Encode())
 	if err != nil {
 		panic(err)
 	}
