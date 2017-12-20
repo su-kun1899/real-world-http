42deeacdea09d69f6a61128b41a00464f24077b2
su-kun1899 <higedrum.coz@gmail.com>
2017-12-20 12:49:23 +0900

    3.9 multipart/form-data形式でファイル送信

---
+++ 3-10_multipartform/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Michael Jackson")
+
+	fileWriter, err := writer.CreateFormFile("thumbnail", "photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+	readFile, err := os.Open("photo.jpg")
+	if err != nil {
+		// ファイル読み込み失敗
+		panic(err)
+	}
+	defer readFile.Close()
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
+	if err != nil {
+		// 送信失敗
+		panic(err)
+	}
+	log.Println("Status: ", resp.Status)
+}
