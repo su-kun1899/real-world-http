2108bfebdfa2c2131d00ee1d2d7f8deb6f416dd9
su-kun1899 <higedrum.coz@gmail.com>
2017-12-22 12:32:11 +0900

    3.11 プロキシの利用

---
+++ 3-15_proxy/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	proxyURL, err := url.Parse("http://localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+	}
+	resp, err := client.Get("http://github.com")
+	if err != nil {
+		panic(err)
+	}
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
